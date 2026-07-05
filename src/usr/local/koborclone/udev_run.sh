68867589aa7af23e73302114a986d0ab47ff6e85
Ben Tinworth <ben.tinworth@gmail.com>
2026-07-05 20:45:31 +1000

    Add missing config load

--- src/usr/local/koborclone/udev_run.sh
+++ src/usr/local/koborclone/udev_run.sh
@@ -6,5 +6,8 @@ if [ "$SETSID" != "1" ]; then
     exit
 fi
 
+# load config
+. "$(dirname "$0")/config.sh"
+
 # run the main script and redirect output to log
 "$KC_HOME/run.sh" > "$LOGS/log.txt" 2>&1 &
